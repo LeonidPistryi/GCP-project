8ddd2ac3f1c25111ed6cfc8952d681bc514a47d5
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-05-15 19:28:13 +0300

    test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "gcp-bucket-sprout-1"
+    bucket = "gcp-team-tf-state-staging"
     prefix = "terraform/state"
     credentials = "credentials_file.json"
   }
--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -6,9 +6,15 @@ REPO_URL="https://packages.cloud.google.com/apt"
         sudo apt-get update -y
         sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
 
-sudo install gcloud components install gke-gcloud-auth-plugin
+# sudo install gcloud components install gke-gcloud-auth-plugin
 
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt-get  install google-cloud-sdk-gke-gcloud-auth-plugin
 gcloud container clusters get-credentials itsprout-k8s-cluster --region us-central1 --project my-first-project-327511
+gcloud components update
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+gke-gcloud-auth-plugin --version 
+
 
 # cd ~
 
