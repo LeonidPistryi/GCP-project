1e23da809112a754e1fd46958dee9aff6c95d70f
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-05-25 14:56:09 +0300

    test1BA

--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -6,7 +6,7 @@ REPO_URL="https://packages.cloud.google.com/apt"
         sudo apt-get update -y
         sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
 
-# sudo install gcloud components install gke-gcloud-auth-plugin and another necassary commands to RUN
+sudo install gcloud components install gke-gcloud-auth-plugin and another necassary commands to RUN
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 sudo apt-get  install google-cloud-sdk-gke-gcloud-auth-plugin
 # gcloud container clusters get-credentials itsprout-k8s-cluster --region us-central1 --project my-first-project-327511
