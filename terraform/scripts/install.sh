6f0c6f15acf112928f1b382ee80b73cda671f2c7
Leonid Pistryi <l.pistryi@it-sprout.org>
2023-05-25 15:07:59 +0300

    test

--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-REPO_URL="https://packages.cloud.google.com/apt"
-        echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list >/dev/null
-        sudo wget -q https://packages.cloud.google.com/apt/doc/apt-key.gpg -O /usr/share/keyrings/cloud.google.gpg
-        sudo apt-get update -y
-        sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
+# REPO_URL="https://packages.cloud.google.com/apt"
+        # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list >/dev/null
+        # sudo wget -q https://packages.cloud.google.com/apt/doc/apt-key.gpg -O /usr/share/keyrings/cloud.google.gpg
+        # sudo apt-get update -y
+        # sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
 
 sudo install gcloud components install gke-gcloud-auth-plugin and another necassary commands to RUN
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
